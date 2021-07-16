7fdbfc0dd6cb145d1b4c0b69353f7895fdd18480
Benjamin <benjamin.pasic1@gmail.com>
2021-07-16 22:18:19 +0200

    Setup most of the logic, last feature added was the change size button

---
+++ app.js
@@ -0,0 +1,50 @@
+let gridContainer = document.querySelector(".grid-container");
+let clearButton = document.querySelector("#clear-button");
+let changeSizeButton = document.querySelector("#change-size-button")
+let root = document.documentElement;
+
+let gridElementsReference;
+
+function addGridItems(size = 16){
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            let element = document.createElement("div");
+            element.classList.add("grid-items");
+            gridContainer.appendChild(element);
+        }
+    }
+    applyEventListeners();
+}
+
+function applyEventListeners(){
+    gridElementsReference = document.querySelectorAll(".grid-items");
+    gridElementsReference.forEach( (element) => {
+        element.addEventListener("mouseover", () => {
+            element.classList.add("black-color");
+        })
+    })
+}
+
+function applyCssGridSize(size = 16){
+    root.style.setProperty("--column-count", size);
+}
+
+clearButton.addEventListener("click", () => {
+    gridElementsReference.forEach( (element) => {
+        element.classList.remove("black-color");
+    })
+})
+
+changeSizeButton.addEventListener("click", () => {
+    let newSize = parseInt(prompt("Enter your new size"));
+    if(isNaN(newSize)){
+        return;
+    }else{
+        gridContainer.innerHTML = "";
+        applyCssGridSize(newSize);
+        addGridItems(newSize);
+    }
+})
+
+applyCssGridSize();
+addGridItems();
