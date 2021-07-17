1a3c6dc490696ededd704d84f706fdfce62879ef
Benjamin <benjamin.pasic1@gmail.com>
2021-07-17 22:48:54 +0200

    Last version of etch-a-sketch

--- app.js
+++ app.js
@@ -1,49 +1,85 @@
 let gridContainer = document.querySelector(".grid-container");
-let clearButton = document.querySelector("#clear-button");
-let changeSizeButton = document.querySelector("#change-size-button")
-let root = document.documentElement;
-let gridElementsReference;
+let colorButtons = document.querySelectorAll(".color-button");
+let optionButtons = document.querySelectorAll(".button");
+let gridElements;
+let color = "Black";
 
-function addGridItems(size = 16){
-    for(let i = 0; i < size; i++){
-        for(let j = 0; j < size; j++){
-            let element = document.createElement("div");
-            element.classList.add("grid-items");
-            gridContainer.appendChild(element);
+function applyGridElements(size = 16){
+    for(let i=0; i<size; i++){
+        for(let j=0; j<size; j++){
+            let div = document.createElement("div");
+            div.classList.add("grid-items");
+            gridContainer.appendChild(div);
         }
     }
-    applyEventListeners();
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridElements = document.querySelectorAll(".grid-items");
+    gridElements.forEach( element => element.addEventListener("mouseover", colorOptions));
 }
 
-function applyEventListeners(){
-    gridElementsReference = document.querySelectorAll(".grid-items");
-    gridElementsReference.forEach( (element) => {
-        element.addEventListener("mouseover", () => {
-            element.classList.add("black-color");
-        })
-    })
-}
-
-function applyCssGridSize(size = 16){
-    root.style.setProperty("--column-count", size);
-}
-
-clearButton.addEventListener("click", () => {
-    gridElementsReference.forEach( (element) => {
-        element.classList.remove("black-color");
-    })
+colorButtons.forEach( button => {
+    button.addEventListener("click", () => {
+        color = button.textContent;
+    });
 })
 
-changeSizeButton.addEventListener("click", () => {
-    let newSize = parseInt(prompt("Enter your new size"));
-    if(isNaN(newSize)){
-        return;
-    }else{
-        gridContainer.innerHTML = "";
-        applyCssGridSize(newSize);
-        addGridItems(newSize);
+optionButtons.forEach( button => {
+    button.addEventListener("click", () => {
+
+        console.log(button.textContent);
+
+        if(button.textContent == "Change size"){
+            let gridSize = Number(prompt("Enter your new grid size (100 max)"));
+
+            if(isNaN(gridSize)){
+                alert("Please enter a number.");
+            }
+
+            if(gridSize > 100 || gridSize < 0){
+                alert("Wrong number input, check your input and try again.");
+            }
+
+            gridContainer.innerHTML = "";
+            applyGridElements(gridSize);
+        }
+        
+        if(button.textContent == "Clear"){
+            gridElements.forEach( element => {
+                element.style.backgroundColor = "white";
+            })
+        }
+    });
+})
+
+function colorOptions(){
+    console.log(color);
+    switch(color){
+        case "Black":
+            this.style.opacity = 1;
+            this.style.backgroundColor = "rgba(0 , 0, 0, 1)";
+            break;
+        case "Rainbow":
+            this.style.opacity = 1;
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            this.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+            break;
+        case "Shading":
+            opacity = parseFloat(this.style.opacity);   
+
+            if(isNaN(opacity) || opacity == 1){
+                this.style.opacity = 0.1;
+                this.style.backgroundColor = "black";
+            }
+
+            if(opacity < 0.9){
+                this.style.opacity = parseFloat(this.style.opacity) + 0.1;
+            }
+
+            break;
     }
-})
+}
 
-applyCssGridSize();
-addGridItems();
+applyGridElements();
+
